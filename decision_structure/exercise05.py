ff403225265d795413c86850aa72e9e039b948f4
Francisco Gomes <arretadolabs@gmail.com>
2024-08-17 00:05:56 -0300

    new commit

---
+++ decision_structure/exercise05.py
@@ -0,0 +1,9 @@
+a = float(input())
+b = float(input())
+average = (a + b) / 2.0
+if average == 10.0:
+    print("approved with distinction")
+elif 7.0 > average > 0.0:
+    print("reproved")
+elif 9.9 >= average >= 7.0:
+    print("approved")
