56eb9b37db736b402d2194e23e12bbd2c9194991
Francisco Gomes <arretadolabs@gmail.com>
2024-08-16 23:29:53 -0300

    new commit

---
+++ sequential_structure/exercise04.py
@@ -0,0 +1,8 @@
+note_one = float(input())
+note_two = float(input())
+note_three = float(input())
+note_four = float(input())
+average_note = (note_one + note_two + note_three + note_four) / 4.0
+print("average: %.2f" % average_note)
+print("second format")
+print("average: %.2f " % ((note_one + note_two + note_three + note_four) / 4.0))
