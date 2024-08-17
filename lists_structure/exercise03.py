51bb6dfa0d5abd22ddf979a68202707b22736bdf
Francisco Gomes <arretadolabs@gmail.com>
2024-08-17 00:29:50 -0300

    new commit

---
+++ lists_structure/exercise03.py
@@ -0,0 +1,12 @@
+list_empty = []
+list_consonant = []
+
+for x in range(0,5):
+    a = str(input())
+    if a == 'a' or a == 'e' or a == 'i' or a == 'o' or a == 'u':
+        list_consonant.append(a)
+
+size_list_consonant = len(list_consonant)
+
+print(list_consonant)
+print(size_list_consonant)
